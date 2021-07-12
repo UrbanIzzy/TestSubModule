85d77c4350b99b0c3207bec9973b3746a560bdc7
UrbanIzzy <izzydevop@gmail.com>
2021-07-12 11:12:37 +0300

    move periph to lib dir

---
+++ libs/periph_sub/uart.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "uart.h"
+
+int  Uart_Init()
+{
+	printf("start uart initialization");
+
+	printf("uart initialization finidh successfully");
+}
+
---
+++ libs/periph_sub/uart.h
@@ -0,0 +1,2 @@
+
+int Uart_Init();
